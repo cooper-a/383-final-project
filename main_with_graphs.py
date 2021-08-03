d8c4f81a86ccaa6a7fd373700d8484cc3176df76
Youming (Mark) Xue <43285947+Markxue923@users.noreply.github.com>
2021-08-02 22:52:43 -0400

    file for graph plotting

--- main_with_graphs.py
+++ main_with_graphs.py
@@ -2,10 +2,12 @@ import numpy as np
 import math
 from scipy.optimize import fsolve
 from loader import Loader
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Constants (Assuming water @ 20°C)
 GRAVITY = 9.81 # UNITS: m/s^2 {L/T^2}
-DENSITY = 998.23 # UNITS: kg/m^3 {M/L^3}
+DENSITY = 998 # UNITS: kg/m^3 {M/L^3}
 VISCOSITY = 1.003e-3 #UNITS: kg/(m*s) {M/(L*T)}
 ROUGHNESS = 0.0025 / 1000 # UNITS: m
 SIN_THETA = 1 / 150
@@ -62,11 +64,36 @@ def get_starting_relative_height(tube_length):
 
 def get_reynolds_number(v2):
     return (DENSITY * v2 * TUBE_DIAMETER) / VISCOSITY
+
+def plot_height_over_time(frames):
+
+    figure, axis = plt.subplots()
+    figure.suptitle("Water Height vs Time")
+    axis.set_xlabel("Time (s)")
+    axis.set_ylabel("Water Height (m)")
     
+    for df in frames:
+        x=df[0]["t"]
+        y=df[0]["h"]
+        
+        if df[2]:
+            label = f"{df[1]}m tube with T-joint"
+        else:
+            label = f"{df[1]}m tube with no T-joint"
+            
+        axis.plot(x, y, label=label)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc="upper right", borderaxespad=0.)
+
+    plt.grid(True)
+    plt.show()
+
 # Run Experiment Method 
 def run_experiment():
+    
+    frames = []
+    columns = ["t", "v1", "v2", "h", "Re"]
     for tube_length_with_t_bool, experimental_time in zip(TUBE_LENGTHS_WITH_T_BOOLS, EXPERIMENTAL_RESULTS):
-        
+        data = []
         tube_length = tube_length_with_t_bool[0]
         k = K_ENTRY if not tube_length_with_t_bool[1] else K_ENTRY + K_TJOINT
         water_height = get_starting_relative_height(tube_length)
@@ -76,17 +103,19 @@ def run_experiment():
         with Loader("Water is draining..."):
             while water_height >= (END_HEIGHT + tube_length * SIN_THETA):
                 v2 = get_v2_velocity(water_height, tube_length, v1, k, friction_coeff) # need to update parameters
+                reynolds_number = get_reynolds_number(v2)
                 friction_coeff = get_friction_coefficient(v2)
                 water_height -= get_change_in_height(v2)
                 v1 = get_v1_velocity(v2)
                 total_time += TIME_INCREMENT
-
+                data.append([total_time, v1, v2, water_height, reynolds_number])
+        df = pd.DataFrame(data=data, columns=columns)
+        df_obj = [df, tube_length, tube_length_with_t_bool[1]]
+        frames.append(df_obj)
         print(f"Predicted time for length of {tube_length * 100}cm is: {total_time:.2f}s")
         print(f"The percent error between predicted and experimental result is {abs((total_time - experimental_time) / experimental_time) * 100:.2f}%")
+    plot_height_over_time(frames)
 
-# Plot Diagram Method
-def plot_diagram():
-    return
 
 if __name__ == "__main__":
     run_experiment()
